0365940e7eb2bd62c342980fde7cc299a8de2780
Max Machado <48817350+maxfmachado@users.noreply.github.com>
2022-02-19 16:17:18 -0300

    Update requisicoes.py

--- requisicoes.py
+++ requisicoes.py
@@ -8,15 +8,14 @@ nomes = []
 lista_com_nome_e_data = []
 url_base = 'https://www.google.com/search?q=data+nascimento+'
 
-# Abrindo e lendo o arquivo json para extrair somente os nomes para a pesquisa:
+# Lendo o arquivo json para extrair somente os nomes para a pesquisa:
 with open("web-scraping\dados.json", encoding='utf-8') as dados_json:
     dados = json.load(dados_json)
 
 for i in dados:
     nomes.append(i['nome'])
 
-# Fazendo a pesquisa da data de nascimento a partir da lista de nomes extraida acima e montando o arquivo excel 
-# com as colunas 'Nome' e 'Data_Nascimento':
+# Fazendo a pesquisa da data de nascimento a partir da lista de nomes extraida acima e montando o arquivo excel com as colunas 'Nome' e 'Data_Nascimento':
 for nome in nomes:
     driver.get(url_base + nome)
     data = driver.find_element_by_xpath("//div[@data-attrid='kc:/people/person:date of birth']/div/div").text
@@ -28,4 +27,4 @@ driver.close()
 
 datas = pd.DataFrame(lista_com_nome_e_data, columns=['Nome', 'Data_Nascimento'])
 print(datas)
-# datas.to_excel('lista_com_datas_nascimento.xlsx')
+datas.to_excel('lista_com_datas_nascimento.xlsx')
